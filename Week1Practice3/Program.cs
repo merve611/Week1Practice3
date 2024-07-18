bbe4c85e3eb135b51877f19d2e053e0e7b532f9a
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-07-18 15:37:48 +0300

    Add project files.

---
+++ Week1Practice3/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+Console.WriteLine(" Rüya Manavına Hoşgeldiniz !");
+Console.WriteLine(" Elma = 2 TL \n Armut = 3 TL \n Çilek = 2 TL \n Muz = 3 TL \n Diğer bütün meyveler = 4 TL");     //defalarca cw yazmamak için \n kullanarak stringleri alt satıra geçirdim
+
+Console.Write(" Hangi meyveyi satın almak istersiniz ? (Elma/Armut/Çilek/Muz/Diğer) : ");
+
+
+string meyve = Console.ReadLine();
+//if (meyve != null)
+//{
+//    if (meyve.ToLower() == "Elma".ToLower())
+//    {
+//        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 2 TL");
+//    }
+//    else if (meyve.ToUpper() == "Armut".ToUpper())
+//    {
+//        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 3 TL");
+//    }
+//    else if (meyve.ToUpper() == "Çilek".ToUpper())
+//    {
+//        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 2 TL");
+//    }
+//    else if (meyve.ToUpper() == "Muz".ToUpper())
+//    {
+//        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 3 TL");
+//    }
+//    else
+//    {
+//        Console.WriteLine("4 TL");
+//    }
+//}     //Bu uygulama için bana göre en mantıklı yöntem switch - case dir. Çünkü birçok olası değer var
+switch (meyve.ToUpper())                                //ToUpper kullanarak girdi de küçükte yazsak duyarsız hale geliyor
+{
+    case "ELMA":
+        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 2 TL");
+        break;
+
+    case "ARMUT":
+        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 3 TL");
+        break;
+
+    case "ÇİLEK":
+        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 2 TL");
+        break;
+
+    case "MUZ":
+        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 3 TL");
+        break;
+ 
+    default:
+        Console.WriteLine(" Seçtiğiniz meyvenin fiyatı : 4 TL");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
